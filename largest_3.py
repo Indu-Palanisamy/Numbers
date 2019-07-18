d5e7bde3357777de12caf00559e285f9a7f69f20
Indu Palanisamy <indu.oct96@gmail.com>
2019-07-18 14:33:12 +0530

    Update largest_3.py

--- largest_3.py
+++ largest_3.py
@@ -1,9 +1,13 @@
-a=int(input("Enter 1st value: "))
-b=int(input("Enter 2nd value: "))
-c=int(input("enter 3rd value: "))
-if(a>b and a>c):
+try:
+ a=int(input("Enter 1st value: "))
+ b=int(input("Enter 2nd value: "))
+ c=int(input("enter 3rd value: "))
+ if(a>b and a>c):
    print(str(a)+" greater than "+str(b)+" and "+str(c))
-elif(b>c and b>a):
+ elif(b>c and b>a):
   print(str(b)+" greater than "+str(a)+" and "+str(c))
-else:
+ else:
   print(str(c)+" greater than "+str(a)+" and "+str(b))
+except ValueError:
+  print("Enter only digits!")  
+
