84c8d7d607d9ca9cacad2af46281df39791443a5
Indu Palanisamy <indu.oct96@gmail.com>
2019-07-18 14:30:50 +0530

    Create largest_3.py

---
+++ largest_3.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st value: "))
+b=int(input("Enter 2nd value: "))
+c=int(input("enter 3rd value: "))
+if(a>b and a>c):
+   print(str(a)+" greater than "+str(b)+" and "+str(c))
+elif(b>c and b>a):
+  print(str(b)+" greater than "+str(a)+" and "+str(c))
+else:
+  print(str(c)+" greater than "+str(a)+" and "+str(b))
